6ab71064f0ef0796c5b10d68a4eee7a64cc16dd4
JaromiZsombi <146178300+JaromiZsombi@users.noreply.github.com>
2025-03-20 10:25:08 +0100

    json beimportálása, header megcsinálása

---
+++ main.js
@@ -0,0 +1,23 @@
+const urlData = 'https://raw.githubusercontent.com/JaromiZsombi/portfolio/refs/heads/master/data.json'
+
+getData(urlData, renderWorks)
+
+function renderWorks(data){
+	console.log(data)
+	document.getElementById('work').innerHTML=''
+	data.forEach(obj=>{
+		document.getElementById('work').innerHTML+=`
+
+		<div class="card">
+  			<img class="work_images" src="${obj.photo_url}" alt="photo" style="width:50%">
+  			<div class="container">
+    			<h4><b>${obj.title}</b></h4>
+    			<p>${obj.topics}</p>
+  			</div>
+		</div>
+		`
+
+		//id = ${obj.id}, title = ${obj.title}, photo = <img src="${obj.photo_url}" alt="">, repo-link = ${obj.repo_link}, topics = ${obj.topics}, site-link = ${obj.site_link}
+	})
+}
+
